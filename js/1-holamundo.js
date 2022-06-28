448ca874085dc9274d0b885e83c5612bc6047232
Enzo A. Heredia <eaheredia292@gmail.com>
2022-06-28 18:04:08 -0300

    agregamos distintos mensajes

---
+++ js/1-holamundo.js
@@ -0,0 +1,17 @@
+//Comentario de Unica Linea
+/*Comentario
+de
+multiples
+líneas*/
+
+// Mostras un mensaje en pantalla o documento
+document.write("<H1>Hola Comision 6i</H1>");
+
+//Mostras un mensaje en consola
+console.log("Prueba de Consola");
+
+//Mostrar mensaje en ventana emergente
+alert("Desde una ventana emergente");
+
+
+
