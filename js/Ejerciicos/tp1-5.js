48dd7775f0065d3aca9b6b89f109fce9165e9115
Enzo A. Heredia <eaheredia292@gmail.com>
2022-06-30 17:31:36 -0300

    Se agregan los Ejercicios de 1 a 5 com ejemmplos de prompt y parseInt

---
+++ js/Ejerciicos/tp1-5.js
@@ -0,0 +1,6 @@
+//5
+let numero1 = prompt("Introduzca un número")
+let numero2 = prompt("Introduzca otro número")
+document.write(parseInt(numero1) + parseInt(numero2))
+console.log(parseInt(numero1))
+console.log(numero1)
