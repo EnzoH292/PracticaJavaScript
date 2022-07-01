4cc4ad30d502dc988a6844ce74aeabf2701a858c
Enzo A. Heredia <eaheredia292@gmail.com>
2022-07-01 01:25:19 -0300

    Ejercicio 6

---
+++ js/Ejercicios/tp1-6.js
@@ -0,0 +1,8 @@
+//6
+let numero1 = parseInt(prompt("Ingrese un número"))
+let numero2 = parseInt(prompt("Ingrese otro número"))
+if (numero1 > numero2){
+    document.write("El mayor valor es " + numero1);
+} 
+
+
