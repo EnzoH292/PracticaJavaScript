51f70c2caddaff139a576ed2cd26848e28a4ecae
Enzo A. Heredia <eaheredia292@gmail.com>
2022-07-05 18:34:00 -0300

    se ve bucles while

---
+++ js/Bucles.js
@@ -0,0 +1,13 @@
+/*
+while (condicion lógica){
+    todas las lineas del codigo que quiero que repita el programa
+    cambiar la condicion lógica
+}
+*/
+
+let cantidad = 0; //cantidad de mates servidos//
+
+while (cantidad <= 20 ){ //20 es la cantidad de veces q se puede servir mates hasta que ya no haya agua//
+    document.write("preparar mate y entregar al siguiente compañero");
+    cantidad = cantidad +1 ; //o cantidad++ // //se modifica la condicion lógica para que no sea un bucle infinito//
+}
