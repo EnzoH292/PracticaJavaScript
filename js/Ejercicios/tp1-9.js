913653c8868521012aa3e9942787f661454a0a24
Enzo A. Heredia <eaheredia292@gmail.com>
2022-07-06 02:37:22 -0300

    se agrega ejercicio 9

---
+++ js/Ejercicios/tp1-9.js
@@ -0,0 +1,31 @@
+let fraseOriginal = prompt("Introduzca una frase de 10 caracteres")
+if (((fraseOriginal.toLowerCase()).charAt(0, 0)) == "a" || ((fraseOriginal.toLowerCase()).charAt(0, 0)) == "e" || ((fraseOriginal.toLowerCase()).charAt(0, 0)) == "i" || ((fraseOriginal.toLowerCase()).charAt(0, 0)) == "o" || ((fraseOriginal.toLowerCase()).charAt(0, 0)) == "u") {
+    console.log(fraseOriginal.charAt(0, 0))
+}
+if (((fraseOriginal.toLowerCase()).charAt(1, 1)) == "a" || ((fraseOriginal.toLowerCase()).charAt(1, 1)) == "e" || ((fraseOriginal.toLowerCase()).charAt(1, 1)) == "i" || ((fraseOriginal.toLowerCase()).charAt(1, 1)) == "o" || ((fraseOriginal.toLowerCase()).charAt(1, 1)) == "u") {
+    console.log(fraseOriginal.charAt(1, 1))
+}
+if (((fraseOriginal.toLowerCase()).charAt(2, 2)) == "a" || ((fraseOriginal.toLowerCase()).charAt(2, 2)) == "e" || ((fraseOriginal.toLowerCase()).charAt(2, 2)) == "i" || ((fraseOriginal.toLowerCase()).charAt(2, 2)) == "o" || ((fraseOriginal.toLowerCase()).charAt(2, 2)) == "u") {
+    console.log(fraseOriginal.charAt(2, 2))
+}
+if (((fraseOriginal.toLowerCase()).charAt(3, 3)) == "a" || ((fraseOriginal.toLowerCase()).charAt(3, 3)) == "e" || ((fraseOriginal.toLowerCase()).charAt(3, 3)) == "i" || ((fraseOriginal.toLowerCase()).charAt(3, 3)) == "o" || ((fraseOriginal.toLowerCase()).charAt(3, 3)) == "u") {
+    console.log(fraseOriginal.charAt(3, 3))
+}
+if (((fraseOriginal.toLowerCase()).charAt(4, 4)) == "a" || ((fraseOriginal.toLowerCase()).charAt(4, 4)) == "e" || ((fraseOriginal.toLowerCase()).charAt(4, 4)) == "i" || ((fraseOriginal.toLowerCase()).charAt(4, 4)) == "o" || ((fraseOriginal.toLowerCase()).charAt(4, 4)) == "u") {
+    console.log(fraseOriginal.charAt(4, 4))
+}
+if (((fraseOriginal.toLowerCase()).charAt(5, 5)) == "a" || ((fraseOriginal.toLowerCase()).charAt(5, 5)) == "e" || ((fraseOriginal.toLowerCase()).charAt(5, 5)) == "i" || ((fraseOriginal.toLowerCase()).charAt(5, 5)) == "o" || ((fraseOriginal.toLowerCase()).charAt(5, 5)) == "u") {
+    console.log(fraseOriginal.charAt(5, 5))
+}
+if (((fraseOriginal.toLowerCase()).charAt(6, 6)) == "a" || ((fraseOriginal.toLowerCase()).charAt(6, 6)) == "e" || ((fraseOriginal.toLowerCase()).charAt(6, 6)) == "i" || ((fraseOriginal.toLowerCase()).charAt(6, 6)) == "o" || ((fraseOriginal.toLowerCase()).charAt(6, 6)) == "u") {
+    console.log(fraseOriginal.charAt(6, 6))
+}
+if (((fraseOriginal.toLowerCase()).charAt(7, 7)) == "a" || ((fraseOriginal.toLowerCase()).charAt(7, 7)) == "e" || ((fraseOriginal.toLowerCase()).charAt(7, 7)) == "i" || ((fraseOriginal.toLowerCase()).charAt(7, 7)) == "o" || ((fraseOriginal.toLowerCase()).charAt(7, 7)) == "u") {
+    console.log(fraseOriginal.charAt(7, 7))
+}
+if (((fraseOriginal.toLowerCase()).charAt(8, 8)) == "a" || ((fraseOriginal.toLowerCase()).charAt(8, 8)) == "e" || ((fraseOriginal.toLowerCase()).charAt(8, 8)) == "i" || ((fraseOriginal.toLowerCase()).charAt(8, 8)) == "o" || ((fraseOriginal.toLowerCase()).charAt(8, 8)) == "u") {
+    console.log(fraseOriginal.charAt(8, 8))
+}
+if (((fraseOriginal.toLowerCase()).charAt(9, 9)) == "a" || ((fraseOriginal.toLowerCase()).charAt(9, 9)) == "e" || ((fraseOriginal.toLowerCase()).charAt(9, 9)) == "i" || ((fraseOriginal.toLowerCase()).charAt(9, 9)) == "o" || ((fraseOriginal.toLowerCase()).charAt(9, 9)) == "u") {
+    console.log(fraseOriginal.charAt(9, 9))
+}
