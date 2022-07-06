acff1a5ee09e3da74e1da795e4fe6f5a2b4744d9
Enzo A. Heredia <eaheredia292@gmail.com>
2022-07-06 01:39:29 -0300

    Se carga ejercicio 8

---
+++ js/Ejercicios/tp1-8.js
@@ -0,0 +1,6 @@
+let numero1 = parseInt(prompt("Introduzca un dividendo"))
+let divisor = 2
+//console.log("El resto es de " + (numero1 % divisor))
+if ( (numero1 % divisor) == 0){
+    document.write("El número " + numero1 + " es divisible en 2")
+}
